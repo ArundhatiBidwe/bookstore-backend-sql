39b0462a13be2318504f0cf5cb605efab1cfe97d
Arundhati Bidwe <>
2024-12-10 03:33:43 -0500

    api issues

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const sequelize = require("./config/database");
-const User = require("./models/User");
-const Video = require("./models/Video");
+const sequelize = require("./config/database.js");
+const User = require("./models/User.js");
+const Video = require("./models/Video.js");
 const app = express();
-const Note = require("./models/Note");
+const Note = require("./models/Note.js");
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // API Routes
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/videos", require("./routes/videos"));
-app.use("/api/notes", require("./routes/notes"));
-
-const PORT = process.env.PORT || 5432;
+app.use("/api/auth", require("./routes/auth.js"));
+app.use("/api/videos", require("./routes/videos.js"));
+app.use("/api/notes", require("./routes/notes.js"));
+app.use("/api/books", require("./routes/books.js"));
+const PORT = process.env.PORT || 4000;
 
 // Sync Database and Start Server
 
